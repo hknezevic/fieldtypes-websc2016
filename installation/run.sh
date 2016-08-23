a26141b6c644fc38dadff80ff35d4ca6558fc422
Edi Modrić <edi@netgen.hr>
2016-08-23 11:29:42 +0200

    Delegate legacy autoloads after DB is created

--- installation/run.sh
+++ installation/run.sh
@@ -1,3 +1,4 @@
 composer install --no-dev -n
 php app/console ezplatform:install clean
 mysql -u summer -pcamp fieldtypes < ezpublish_legacy/extension/eztags/sql/mysql/schema.sql
+php app/console ezpublish:legacy:script bin/php/ezpgenerateautoloads.php
